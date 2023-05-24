cfa42739b80de962cc9b05f1f19483fadb03ff1b
Yuri Melo <yurifullstack@gmail.com>
2023-05-24 12:54:15 -0300

    Create static method for generating password

--- src/Core/Entity/Password.ts
+++ src/Core/Entity/Password.ts
@@ -1,14 +1,32 @@
-export default class Password {
-    private value: string;
+import { pbkdf2, randomBytes } from "crypto";
 
-    constructor (password: string) {
+export default class Password {
+    private readonly _value: string;
+    private readonly _salt: string | undefined;
+
+    constructor (password: string, salt?: string) {
 
         if (password.length < 8) throw new Error("Invalid parameter");
 
-        this.value = password;
+        this._value = password;
+        this._salt = salt;
+    }
+
+    static async Factory(password: string, salt?: string): Promise<Password> {
+        const generatedSalt = salt || randomBytes(20).toString("hex");
+
+        return new Promise((resolve) => {
+            pbkdf2(password, generatedSalt, 100, 64, "sha512", (error, value) => {
+                resolve(new Password(value.toString("hex"), generatedSalt));
+            });
+        });
     }
 
     getValue(): string {
-        return this.value;
+        return this._value;
+    }
+
+    getSalt(): string | undefined {
+        return this._salt;
     }
 }
