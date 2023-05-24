c9429595826b9da854900929b5130cfd7496f078
Yuri Melo <yurifullstack@gmail.com>
2023-05-24 12:54:47 -0300

    test: create password

--- test/unit/Password.test.ts
+++ test/unit/Password.test.ts
@@ -1,10 +1,10 @@
 import Password from "../../src/core/entity/Password";
 
-test("Deve criar senha com 8 ou mais chars", function () {
-    const password = new Password("12345678");
-    expect(password.getValue()).toBe("12345678");
+test("Deve criar senha com 8 ou mais chars", async function () {
+    const password = await Password.Factory("12345678", "salt");
+    expect(password.getValue()).toBe("f1fa680348802c16e610e0afa109ef9fd2ea21001bf0449ea4372229cee93a13c3eb08a30068a92b82d376d195f5ed4bebfd9b51413a0ae23dbb38da9141a4b4");
+    expect(password.getSalt()).toBe("salt");
 });
-
 
 test("Não deve criar senha com menos que 8 chars", function () {
     expect(() => new Password("1234567")).toThrow(new Error("Invalid parameter"));
